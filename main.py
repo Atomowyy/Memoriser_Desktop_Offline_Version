5ac8e4a0e2b104e5b62c6b18b588569417c84329
Atomowyy <wojciech.lewkoo@gmail.com>
2024-09-17 21:45:25 +0200

    Menu cosmetics

--- main.py
+++ main.py
@@ -1,16 +1,34 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
+#Nothing important, just cosmetics
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    print("-----------------------------------------------------------------------\n"
+          "___  ___                          _               _   _  __   _____ \n"
+          "|  \\/  |                         (_)             | | | |/  | |  _  ||\n"
+          "| .  . | ___ _ __ ___   ___  _ __ _ ___  ___ _ __| | | |`| | | |/' |\n"
+          "| |\\/| |/ _ \\ '_ ` _ \\ / _ \\| '__| / __|/ _ \\ '__| | | | | | |  /| |\n"
+          "| |  | |  __/ | | | | | (_) | |  | \\__ \\  __/ |  \\ \\_/ /_| |_\\ |_/ /\n"
+          "\\_|  |_/\\___|_| |_| |_|\\___/|_|  |_|___/\\___|_|   \\___/ \\___(_)___/\n"
+          "                                            ______\n"
+          "                                           |______|\n"
+          "-----------------------------------------------------------------------")
+    print(
+        '     Welcome to Memoriser Offline Desktop Version V1.0 by Atomowyy\n------------------------------------------'
+        '-----------------------------')
+    while True:
+        print('1. Editing mode\n2. Learning mode\n3. Exit')
+        try:
+            user_choice: int = int(input('Make your choice: '))
+        except ValueError:
+            print('You\'ve entered wrong option, try again.')
+        else:
+            match user_choice:
+                case 1:
+                    print('Editing mode. Work in progress...')
+                case 2:
+                    print('Learning mode. Work in progress...')
+                case 3:
+                    print('EXIT IS ALSO WORK IN PROGRESS, YOU CANT ESCAPE')
+                    print('just joking')
+                    break
+                case _:
+                    print("why")
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
